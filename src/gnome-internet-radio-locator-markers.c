26d8ab96be7a8aaa45589d95cd5775d259448b3f
Ole Aamot <ole@gnome.org>
2019-02-03 17:02:42 +0100

    Fix Boston marker

--- src/gnome-internet-radio-locator-markers.c
+++ src/gnome-internet-radio-locator-markers.c
@@ -171,7 +171,7 @@ create_marker_layer (G_GNUC_UNUSED ChamplainView *view, ChamplainPathLayer **pat
   /* champlain_path_layer_add_node (*path, CHAMPLAIN_LOCATION (marker)); */
   g_signal_connect(CHAMPLAIN_LOCATION(marker), "button-press", G_CALLBACK(marker_function), station);
   marker = champlain_label_new_from_file ("icons/emblem-generic.png", NULL);
-  station = g_strdup("WTBU, Boston, Massachusetts\n<span size=\"small\">http://www.wtburadio.org/</span>");
+  station = g_strdup("Boston, Massachusetts\n<span size=\"small\">http://www.wtburadio.org/</span>");
   champlain_label_set_text (CHAMPLAIN_LABEL (marker), station);
   champlain_label_set_use_markup (CHAMPLAIN_LABEL (marker), TRUE);
   champlain_label_set_color (CHAMPLAIN_LABEL (marker), &city_color);
