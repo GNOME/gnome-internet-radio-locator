2543ce5ce3abe80fd06eefee976df35f9477bcf3
Ole Aamot <ole@gnome.org>
2020-09-06 01:19:15 +0200

    GNOME Internet Radio Locator version 3.1.0

--- src/gnome-internet-radio-locator.c
+++ src/gnome-internet-radio-locator.c
@@ -20,6 +20,7 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+#include <config.h>
 #include <gtk/gtk.h>
 #include <gst/player/player.h>
 #include <champlain/champlain.h>
